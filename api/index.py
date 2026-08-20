2ccc634a8ed79468a0c93d702e058a5acb55ef47
Skyjoe <hairjoe@gmail.com>
2026-08-20 17:02:04 -0300

    Enable threaded mode for API server

--- api/index.py
+++ api/index.py
@@ -311,4 +311,4 @@ if __name__ == '__main__':
     print('=' * 60)
     print('Radio Prime API Server')
     print('=' * 60)
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    app.run(debug=True, host='0.0.0.0', port=5000, threaded=True)

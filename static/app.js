0e07f32e47e1e0ec26fc259fde3209c6444a14c4
Skyjoe <hairjoe@gmail.com>
2026-09-05 21:02:44 -0300

    Update app.js

--- static/app.js
+++ static/app.js
@@ -1056,6 +1056,7 @@ function mostrarResultado(track, fonte) {
             const metaElement = document.createElement("div");
             metaElement.className = "shazam-meta";
             metaElement.textContent = album && ano ? `album⋅{album} ·album⋅{ano}` : (album || ano);
+            
             info.appendChild(metaElement);
         }
     }
